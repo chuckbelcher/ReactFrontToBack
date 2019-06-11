8e4e0aa5e9e2d63ddda989bb962abd503f8f361b
Chuck Belcher <cfbelcher@pateogroup.com>
2019-06-10 21:24:34 -0400

    Added bootstrap and contacts component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
@@ -10,14 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="Phil Bunton"
-          email="pbutton@buttons.com"
-          phone="444-555-6666"
-        />
-        <Contact name="Penny Bikel"
-          email="penny@sunshine.com"
-          phone="444-777-2222"
-        />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
