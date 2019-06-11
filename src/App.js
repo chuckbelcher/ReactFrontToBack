f3a2d42a543dda20ed3b1084adeb105e55bb416f
Chuck Belcher <cfbelcher@pateogroup.com>
2019-06-10 21:59:33 -0400

    Finished Events in React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'
 import './App.css';
 
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+    state = { showContactInfo: true }
 
     static propTypes = {
         contact: PropTypes.object.isRequired
     };
 
+    onShowClick = (e) => {
+        //showContactInfo = !showContactInfo;
+        console.log(e, this.state);
+    }
+
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { id, name, email, phone } = this.props.contact;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>{name} <i onClick={this.onShowClick.bind(this, { id })} className="fas fa-sort-down"></i></h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone} </li>
