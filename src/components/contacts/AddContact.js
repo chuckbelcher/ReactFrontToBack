4b33ac24b07f6a400f309c24dc4747e35dc4946d
Charles Belcher <cfbelcher@pateogroup.com>
2019-07-03 20:55:04 -0400

    Completed Form Error Checking and Display

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,23 +1,34 @@
-import React, { Component } from "react";
-import { Consumer } from "../../context";
-import TextInputGroup from "../layout/TextInputGroup";
-import uuid from "uuid";
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
-    name: "",
-    email: "",
-    phone: ""
-  };
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    //Check fields to ensure that they are populated
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is a required field' } });
+      console.log(this.state);
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is a required field' } });
+      console.log(this.state);
+      return;
+    }
+
     const newContact = {
       id: uuid(),
       name,
@@ -25,18 +36,23 @@ class AddContact extends Component {
       phone
     };
 
-    dispatch({ type: "ADD_CONTACT", payload: newContact });
-
     //Clear state to clear form elements after submission
     this.setState({
-      name: "",
-      email: "",
-      phone: ""
+      name: '',
+      email: '',
+      phone: '',
+      errors: ''
     });
+
+    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -44,7 +60,7 @@ class AddContact extends Component {
           const { dispatch } = value;
           return (
             <div className="card mb-3">
-              <div className="card-header"> Add Contact</div>
+              <div className="card-header bg-dark text-white"> Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
@@ -53,6 +69,7 @@ class AddContact extends Component {
                     placeholder="Enter Name"
                     value={name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
@@ -61,6 +78,7 @@ class AddContact extends Component {
                     placeholder="Enter E-Mail"
                     value={email}
                     onChange={this.onChange}
+                    error={errors.email}
                   />
                   <TextInputGroup
                     label="Phone Number"
@@ -68,8 +86,8 @@ class AddContact extends Component {
                     placeholder="Enter Telephone Number"
                     value={phone}
                     onChange={this.onChange}
+                    error={errors.phone}
                   />
-
                   <input
                     type="submit"
                     value="Add Contact"
